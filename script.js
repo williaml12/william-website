5cb77b21a0b920b45c91cfbabbc65959172d225a
William Lu <32563160+williaml12@users.noreply.github.com>
2024-08-09 17:11:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,35 +10,3 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
-
-// Element Fade-in on Scroll
-const faders = document.querySelectorAll('.fade-in');
-
-const appearOptions = {
-    threshold: 0,
-    rootMargin: '0px 0px -200px 0px'
-};
-
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add('appear');
-            appearOnScroll.unobserve(entry.target);
-        }
-    });
-}, appearOptions);
-
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
-});
-
-// Dark Mode Toggle
-const themeToggle = document.getElementById('theme-toggle');
-const body = document.body;
-
-themeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    themeToggle.textContent = body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
-});
